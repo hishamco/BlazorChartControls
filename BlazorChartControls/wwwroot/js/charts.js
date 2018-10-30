da1a3010fc2c1eeebc64e801e5113d11f9f48a83
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2018-10-30 09:32:22 +0300

    Create charts APIs that utilize morris charts

---
+++ BlazorChartControls/wwwroot/js/charts.js
@@ -0,0 +1,35 @@
+﻿window.charts = {
+    drawLineChart: function (id, dataSource, xKey, yKeys, labels) {
+        Morris.Line({
+            element: id,
+            data: dataSource,
+            xkey: xKey,
+            ykeys: yKeys,
+            labels: labels
+        });
+    },
+    drawAreaChart: function (id, dataSource, xKey, yKeys, labels) {
+        Morris.Area({
+            element: id,
+            data: dataSource,
+            xkey: xKey,
+            ykeys: yKeys,
+            labels: labels
+        });
+    },
+    drawBarChart: function (id, dataSource, xKey, yKeys, labels) {
+        Morris.Bar({
+            element: id,
+            data: dataSource,
+            xkey: xKey,
+            ykeys: yKeys,
+            labels: labels
+        });
+    },
+    drawDonutChart: function (id, dataSource) {
+        Morris.Donut({
+            element: id,
+            data: dataSource
+        });
+    }
+};
